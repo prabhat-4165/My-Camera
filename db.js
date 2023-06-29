533bd1b7046a0baf7bce374df7557fe04c850eb6
prabhat <prabhu27082001@gmail.com>
2023-06-29 17:54:57 +0530

    created openRequest to connect it with indexdb

---
+++ db.js
@@ -0,0 +1,11 @@
+let db;
+let openRequest = indexedDB.open("myDataBase");
+openRequest.addEventListener("success", (e)=>{
+
+})
+openRequest.addEventListener("error", (e)=>{
+    
+})
+openRequest.addEventListener("upgradeneeded", (e)=>{
+    
+})
