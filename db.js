9643e7575de77504018f9ee4e2ad7a1300564ccd
prabhat <prabhu27082001@gmail.com>
2023-08-28 16:27:20 +0530

    updated db

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
+// open database
+// create onjectstore
+
 let db;
 let openRequest = indexedDB.open("myDataBase");
 openRequest.addEventListener("success", (e)=>{
-
+   console.log("DB Success");
+   db = openRequest.result;
 })
 openRequest.addEventListener("error", (e)=>{
-    
+    console.log("DB Erorr");
 })
 openRequest.addEventListener("upgradeneeded", (e)=>{
-    
+    console.log("DB upgraded and also for initial db creation");
+    db = openRequest.result;
+
+// object store can only be created in upgradeneeded
+ db.createObjectStore("video", {keyPath: "id"});
+ db.createObjectStore("image", {keyPath: "id"});
+
 })
